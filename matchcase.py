3c351173cc014d85fbb176e27662cce9d9f337cd
arantza <arantzafe@ieslamarisma,net>
2025-09-30 00:41:04 +0200

    cambio

---
+++ matchcase.py
@@ -0,0 +1,13 @@
+mes = 4
+
+match mes:
+    case 12 | 1 | 2:
+        print("Invierno")
+    case 3 | 4 | 5:
+        print("Primavera")
+    case 6 | 7 | 8:
+        print("Verano")
+    case 9 | 10 | 11:
+        print("Otoño")
+    case _:
+        print("Cualquier otro valor")
